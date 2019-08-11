ec697187fed822efcbe067aa35e431295f75b80e
ssamwonn <ssamwonn@ssamwonnui-MacBook-Pro.local>
2019-08-11 13:56:34 +0900

    init

---
+++ src/Movie.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './Movie.css';
+
+class Movie extends Component {
+    render() {
+        return (
+            <h1>hello it this movie </h1>
+        )
+    }
+}
+
+
+export default Movie  
